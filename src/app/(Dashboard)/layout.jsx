2967613a76ca13c98b4861ebb64a2103cd8847d2
jawwadkhan777 <m.jawwadkhan777@gmail.com>
2026-01-28 12:13:42 +0500

    change layout structure

--- src/app/(Dashboard)/layout.jsx
+++ src/app/(Dashboard)/layout.jsx
@@ -14,13 +14,16 @@ export default function DashboardLayout({ children }) {
       <div className="flex flex-col flex-1">
         
         {/* Navbar */}
-        <header className="h-16 ">
+        <header className="h-16">
           <Navbar />
         </header>
 
         {/* Page Content */}
-        <main className="flex-1 p-6 overflow-y-auto">
-          {children}
+        <main className="flex-1 p-4 overflow-y-auto bg-gray-100 dark:bg-gray-900">
+          {/* Card-like container */}
+          <div className="bg-blue-100/50 rounded-xl shadow-lg p-6 min-h-full">
+            {children}
+          </div>
         </main>
 
       </div>
